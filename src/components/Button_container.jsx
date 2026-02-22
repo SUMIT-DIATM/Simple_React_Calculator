65a27f500ff9c04a91780bdb6f482b76d43ce74f
sumit <cs2460.diatm@gmail.com>
2026-02-22 13:57:26 +0530

    code update

--- src/components/Button_container.jsx
+++ src/components/Button_container.jsx
@@ -1,7 +1,13 @@
 import styles from "../css/Button_container.module.css"
 import Buttons from "./Buttons";
 function Button_container({calval,updatedisplay}){
-    const elements = [`AC`,"+",`1`,`2`,`3`,"-",`4`,`5`,`6`,"*",`7`,`8`,`9`,`.`,"/",`0`,'DEL','00',"="];
+    const elements = [
+  "AC", "1", "2", "+",
+  "3", "4", "5", "-",
+  "6", "7", "8", "*",
+  "9", ".", "0", "/",
+  "DEL", "00", "="
+];
 return <>
     <div className={styles.btn_container}>{elements.map((ele)=><Buttons  ele={ele} calval={calval} updatedisplay={updatedisplay} key={ele} ></Buttons>)}</div>
 </>
